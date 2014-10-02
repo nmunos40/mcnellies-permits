6d59b258738052e94a50117c5f5f8e9738f71ff2
Ben Munos <benmunos@gmail.com>
2014-10-02 16:19:59 -0500

    forgot to push css issue for alyvia

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 	  user = User.find_by_email(params[:email])
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
-	    redirect_to site_permits_path, notice: "Logged in!"
+	    redirect_to site_permits_path
 	  else
 	    flash.now.alert = "Email or password is invalid"
 	    render "new"
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 	  session[:user_id] = nil
-	  redirect_to site_permits_path, notice: "Logged out!"
+	  redirect_to site_permits_path
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	validates_uniqueness_of :email
+	before_save { |user| user.email = email.downcase }
+	validates :password, confirmation: true, length: { minimum: 6, unless: "password.blank?", too_short: "is too short (must be at least 6 characters)" }
 end
